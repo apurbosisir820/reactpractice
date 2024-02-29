0526d84ffb8a6eed40bd712a8838dc4b657f0705
apurbosisir820 <www.alaminvainboy58@gmail.com>
2024-03-01 02:36:13 +0600

    reduce function

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let a = 10
 let b = 5
 let c = a +b
 console.log(c);
-let amin = ['amin', 'sisir','apur']
-let splits = amin.split(' ')
-console.log(splits)
+// let amin = 'amin sisir apur'
+// let splits = amin.split("")
+let sisir = [3,4,5,6,11]
+let tub = sisir.reduce((ka,b)=>ka+b)
+console.log(tub)
